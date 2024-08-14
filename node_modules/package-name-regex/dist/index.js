55a5a822485748cd4ec04569c4b0c64168ebbb98
th13vn <th13ntc@gmail.com>
2024-08-14 11:31:13 +0700

    ud version

--- node_modules/package-name-regex/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-var _default = /^(@[a-z0-9-~][a-z0-9-._~]*\/)?[a-z0-9-~][a-z0-9-._~]*$/;
-exports.default = _default;
-module.exports = exports.default;
